c404a8e0debd4dfdc3fbef4ff388006cd167f0fb
ben300694 <s6berupp@uni-bonn.de>
2018-05-30 20:06:58 +0200

    Tried reading in geotiff

--- readGeotiff.m
+++ readGeotiff.m
@@ -1,6 +1,8 @@
 geotiffFile = '/media/data/bruppik/Pix4D/example_cadastre/Downloaded_from_Pix4DMapper/Cadastre_dsm.tif';
 
-info = geotiffinfo(geotiffFile);
-disp(info);
+%info = geotiffinfo(geotiffFile);
+%disp(info);
 
-[image, R] = geotiffread(geotiffFile);
+%[image, R] = geotiffread(geotiffFile);
+
+image = GEOTIFF_READ(geotiffFile);
