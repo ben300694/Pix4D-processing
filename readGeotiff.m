3f9e096e68b2f8b4eae59f6e6f72ae6bfcf5d9f4
ben300694 <s6berupp@uni-bonn.de>
2018-05-30 20:00:00 +0200

    Added a MATLAB file

---
+++ readGeotiff.m
@@ -0,0 +1,6 @@
+geotiffFile = '/media/data/bruppik/Pix4D/example_cadastre/Downloaded_from_Pix4DMapper/Cadastre_dsm.tif';
+
+info = geotiffinfo(geotiffFile);
+disp(info);
+
+[image, R] = geotiffread(geotiffFile);
